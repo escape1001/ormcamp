d2ed654e5354a6476c5572303639585e9dbadee9
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-02-28 10:06:34 +0900

    "study" 0227 cbv 예제 admin 커스텀

--- django/0227/cbv/blog/admin.py
+++ django/0227/cbv/blog/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+''' admin 커스텀 하고 싶을 때
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'created_at', 'updated_at']
+    # fields = ['title', 'content'] # 이전 버전에서는 fields를 사용했습니다.
+
+admin.site.register(Post, PostAdmin)
+'''
